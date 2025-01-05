929c99a7384e20dbc95273dc84f3d4fdbbd6bb56
Bharath-Kumar-S <Bharath-Kumar-S@users.noreply.github.com>
2025-01-05 18:01:48 +0000

    deploy: 8c8f1fa2c7c21335e39cd972c2c72f36ebab30c8

---
+++ assets/Button.stories-XuAZgzoW.js
@@ -0,0 +1,39 @@
+import{f as F}from"./index-Dj6nxAlZ.js";import{j as m}from"./jsx-runtime-BjgbQsUx.js";import"./index-D2MAbzvX.js";const E=({label:M,onClick:P,disabled:U=!1,size:W="md",variant:$="primary",icon:l,...O})=>m.jsxs("button",{className:`button-${$} button-${W} button-hover disabled:opacity-50 disabled:cursor-not-allowed inline-flex items-center justify-center gap-2`,disabled:U,onClick:P,...O,children:[l&&m.jsx("span",{children:l}),M]});E.__docgenInfo={description:"",methods:[],displayName:"Button",props:{label:{required:!0,tsType:{name:"string"},description:""},onClick:{required:!0,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},variant:{required:!1,tsType:{name:"union",raw:"'primary' | 'secondary' | 'tertiary' | 'danger'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'tertiary'"},{name:"literal",value:"'danger'"}]},description:"",defaultValue:{value:"'primary'",computed:!1}},size:{required:!1,tsType:{name:"union",raw:"'sm' | 'md' | 'lg'",elements:[{name:"literal",value:"'sm'"},{name:"literal",value:"'md'"},{name:"literal",value:"'lg'"}]},description:"",defaultValue:{value:"'md'",computed:!1}},icon:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const K={title:"Components/Button",component:E,parameters:{layout:"centered"},tags:["autodocs"],args:{label:"Button",onClick:F()}},e={args:{variant:"primary"}},r={args:{variant:"secondary"}},a={args:{variant:"tertiary"}},s={args:{size:"sm"}},t={args:{size:"md"}},n={args:{size:"lg"}},o={args:{disabled:!0}},i={args:{icon:"👍",label:"Thumbs Up"}},c={args:{variant:"danger",label:"Delete"}};var u,d,p;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary'
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var g,y,b;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(b=(y=r.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var v,f,B;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: 'tertiary'
+  }
+}`,...(B=(f=a.parameters)==null?void 0:f.docs)==null?void 0:B.source}}};var S,T,h;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    size: 'sm'
+  }
+}`,...(h=(T=s.parameters)==null?void 0:T.docs)==null?void 0:h.source}}};var x,z,q;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    size: 'md'
+  }
+}`,...(q=(z=t.parameters)==null?void 0:z.docs)==null?void 0:q.source}}};var D,j,w;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    size: 'lg'
+  }
+}`,...(w=(j=n.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};var R,L,N;o.parameters={...o.parameters,docs:{...(R=o.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(N=(L=o.parameters)==null?void 0:L.docs)==null?void 0:N.source}}};var _,C,I;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    icon: '👍',
+    label: 'Thumbs Up'
+  }
+}`,...(I=(C=i.parameters)==null?void 0:C.docs)==null?void 0:I.source}}};var V,k,A;c.parameters={...c.parameters,docs:{...(V=c.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger',
+    label: 'Delete'
+  }
+}`,...(A=(k=c.parameters)==null?void 0:k.docs)==null?void 0:A.source}}};const Q=["Primary","Secondary","Tertiary","SmallButton","MediumButton","LargeButton","DisabledButton","ButtonWithIconAndLabel","DangerButton"];export{i as ButtonWithIconAndLabel,c as DangerButton,o as DisabledButton,n as LargeButton,t as MediumButton,e as Primary,r as Secondary,s as SmallButton,a as Tertiary,Q as __namedExportsOrder,K as default};
